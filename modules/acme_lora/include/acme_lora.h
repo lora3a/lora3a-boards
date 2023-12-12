5483cbbf85107ba66067e13ae502146b26fef1c2
Michele Forese <michele.forese.personal@gmail.com>
2023-12-12 10:59:55 +0100

    feat: added DEFAULT_LORA_BOOST to acme_lora module

--- modules/acme_lora/include/acme_lora.h
+++ modules/acme_lora/include/acme_lora.h
@@ -40,17 +40,21 @@
 #define DEFAULT_LORA_POWER SX127X_RADIO_TX_POWER
 #endif
 
-typedef void(lora_data_cb_t)(const char *buffer, size_t len, int16_t *rssi,
-                             int8_t *snr);
+#ifndef DEFAULT_LORA_BOOST
+#define DEFAULT_LORA_BOOST SX127X_PA_BOOST
+#endif
+
+typedef void (lora_data_cb_t)(const char *buffer, size_t len, int16_t *rssi,
+                              int8_t *snr);
 
 typedef struct {
-  uint8_t bandwidth;
-  uint8_t spreading_factor;
-  uint8_t coderate;
-  uint32_t channel;
-  int16_t power;
-  uint8_t boost;
-  lora_data_cb_t *data_cb;
+    uint8_t bandwidth;
+    uint8_t spreading_factor;
+    uint8_t coderate;
+    uint32_t channel;
+    int16_t power;
+    uint8_t boost;
+    lora_data_cb_t *data_cb;
 } lora_state_t;
 
 int lora_init(const lora_state_t *state);
